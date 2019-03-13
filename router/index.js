d3644fc7e32dcc36b8b128d1598744020ab8c6bb
nfeng <308561157@qq.com>
2019-03-13 18:30:41 +0800

    再设置一下

--- router/index.js
+++ router/index.js
@@ -7,6 +7,18 @@ const router = new Router({
     // prefix: '/api'
 })
 
+router.all('*', (ctx, next) => {
+    // 允许来自所有域名请求
+    ctx.set('Access-Control-Allow-Origin', '*');
+    // 是否允许发送Cookie，ture为运行
+    ctx.set('Access-Control-Allow-Credentials', true);
+    // 设置所允许的HTTP请求方法
+    ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
+    // 服务器支持的所有头信息字段，多个字段用逗号分隔
+    ctx.set('Access-Control-Allow-Headers', 'x-requested-with, x-ui-request, lang');
+    next();
+})
+
 router.get('/', async function (ctx, next) {
     const filePath = './views/index.html'
     ctx.sendHtmlFile(filePath)
