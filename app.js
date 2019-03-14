ba2066e11cc34cb2a8a5f436607bc52ea51c59b6
nfeng <308561157@qq.com>
2019-03-14 11:17:35 +0800

    跨域设置

--- app.js
+++ app.js
@@ -23,9 +23,21 @@ if (Config.debug) {
 }
 
 // 跨域
-// if (process.env.CORS === 'true') {
-//     app.use(KoaCors())
-// }
+if (process.env.CORS === 'true') {
+    app.use(KoaCors({
+        origin: function (ctx) {
+            // if (ctx.url === '/test') {
+                return "*"; // 允许来自所有域名请求
+            // }
+            // return "http://localhost:8080"; // 这样就能只允许 http://localhost:8080 这个域名的请求了
+        },
+        exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+        maxAge: 5,
+        credentials: true, // 当设置成允许请求携带cookie时，需要保证"Access-Control-Allow-Origin"是服务器有的域名，而不能是"*";
+        allowMethods: ['GET', 'POST', 'DELETE'],
+        allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    }))
+}
 
 app.use(bodyParser());
 
