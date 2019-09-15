901c65f8a85b498af11e0c3047751ce3c141d432
nfeng <308561157@qq.com>
2019-09-16 00:51:49 +0800

    先睡觉了

--- router/admin/index.js
+++ router/admin/index.js
@@ -1,60 +1,61 @@
-const Router = require('koa-router')
-const role_router = require('./role') // 角色
-const user_router = require('./auth')  // 用户
-const img_router = require('./img') // 图片
-const task_router = require('./task') // 任务
-const note_router = require('./note') // 笔记
-const global_router = require('./global') // 全部
-const jwt = require('jsonwebtoken')
-const baseUrl = '/api'
+const Router = require("koa-router");
+const role_router = require("./role"); // 角色
+const user_router = require("./auth"); // 用户
+const img_router = require("./img"); // 图片
+const task_router = require("./task"); // 任务
+const note_router = require("./note"); // 笔记
+const global_router = require("./global"); // 全部
+const jwt = require("jsonwebtoken");
+const baseUrl = "/api";
 
 function getUser(TokenData) {
   return new Promise((resolve, reject) => {
     jwt.verify(TokenData, process.env.JWT_KEY, function (err, decoded) {
       if (err) {
         console.log(err);
-        reject({ code: 401, msg: 'token 过时' });
+        reject({ code: 401, msg: "token 过时" });
       } else {
         resolve(decoded.data);
       }
-    })
+    });
   });
 }
 
 const router = new Router({
   prefix: baseUrl
-})
+});
 
 // 数据拦截判断用户类型
-router.all('*', async (ctx, next) => {
-  const indexOf = ctx.request.url.indexOf('?')
-  const url = indexOf === -1 // 区分get/post请求 
-    ? ctx.request.url.replace(baseUrl, '') // post请求拿到的地址
-    : ctx.request.url.substring(baseUrl.length, indexOf) // get请求拿到的地址
-  const whiteList = ['/auth/login', '/auth/signup'] // 白名单
+router.all("*", async (ctx, next) => {
+  const indexOf = ctx.request.url.indexOf("?");
+  const url =
+    indexOf === -1 // 区分get/post请求
+      ? ctx.request.url.replace(baseUrl, "") // post请求拿到的地址
+      : ctx.request.url.substring(baseUrl.length, indexOf); // get请求拿到的地址
+  const whiteList = ["/auth/login", "/auth/signup", "/note/list", "/note/detail"]; // 白名单
   if (whiteList.some(item => item === url)) {
-    await next()
-    return
+    await next();
+    return;
   }
-  const TokenKey = 'Nfeng-Token'
-  const TokenData = ctx.cookies.get(TokenKey)
-  const infoData = await getUser(TokenData)
+  const TokenKey = "Nfeng-Token";
+  const TokenData = ctx.cookies.get(TokenKey);
+  const infoData = await getUser(TokenData);
   if (!infoData) {
-    throw { code: 500, msg: '角色不存在' };
+    throw { code: 500, msg: "角色不存在" };
   } else {
     if (infoData.permissions.some(item => item === url)) {
-      await next()
+      await next();
     } else {
-      throw { code: '500', msg: '您没有权限访问此接口哦~' }
+      throw { code: "500", msg: "您没有权限访问此接口哦~" };
     }
   }
-})
+});
 
-router.use(user_router.routes(), user_router.allowedMethods())
-router.use(role_router.routes(), role_router.allowedMethods())
-router.use(img_router.routes(), img_router.allowedMethods())
-router.use(task_router.routes(), task_router.allowedMethods())
-router.use(note_router.routes(), note_router.allowedMethods())
-router.use(global_router.routes(), global_router.allowedMethods())
+router.use(user_router.routes(), user_router.allowedMethods());
+router.use(role_router.routes(), role_router.allowedMethods());
+router.use(img_router.routes(), img_router.allowedMethods());
+router.use(task_router.routes(), task_router.allowedMethods());
+router.use(note_router.routes(), note_router.allowedMethods());
+router.use(global_router.routes(), global_router.allowedMethods());
 
-module.exports = router
+module.exports = router;
--- router/admin/note.js
+++ router/admin/note.js
@@ -1,75 +1,75 @@
-const Router = require('koa-router');
-const NoteMongodb = require('../../mongodb/Note');
+const Router = require("koa-router");
+const NoteMongodb = require("../../mongodb/note");
 
 const router = new Router({
-  prefix: '/note'
+  prefix: "/note"
 });
 
-router.post('/add', async (ctx) => {
-  ctx.isStrings(['title', 'content']);
+router.post("/add", async ctx => {
+  ctx.isStrings(["title", "content"]);
   const { title, content } = ctx.vals;
   const NoteData = await NoteMongodb.add(title, content);
   const resData = {
     NoteId: NoteData._id,
     title: NoteData.title,
-    content: NoteData.content,
-  }
+    content: NoteData.content
+  };
   const data = {
-    ...resData,
-  }
-  ctx.sendSuccess(data, '创建成功!');
-})
-
-router.post('/delete', async (ctx) => {
-  ctx.isStrings(['noteId']);
-  const { noteId } = ctx.vals;
-  const taskData = await NoteMongodb.delete(noteId);
-  ctx.sendSuccess(taskData, '删除成功!');
+    ...resData
+  };
+  ctx.sendSuccess(data, "创建成功!");
 });
 
-router.post('/update', async (ctx) => {
-  ctx.isStrings(['noteId', 'title', 'content'])
-  const { noteId, title, content } = ctx.vals
-  if (!await NoteMongodb.find(noteId)) {
-    throw { code: 10002, msg: '笔记不存在' }
-  }
-  const updateTime = new Date().getTime()
-  const taskData = await NoteMongodb.update(noteId, title, content, updateTime)
-  if (!taskData.ok) {
-    throw { code: 500, msg: '修改失败~' }
-  }
-  ctx.sendSuccess('', '修改成功~')
-})
+router.post("/delete", async ctx => {
+  ctx.isStrings(["noteId"]);
+  const { noteId } = ctx.vals;
+  const taskData = await NoteMongodb.delete(noteId);
+  ctx.sendSuccess(taskData, "删除成功!");
+});
 
-router.post('/detail', async (ctx) => {
-  ctx.isStrings(['noteId'])
-  const { noteId } = ctx.vals
-  const taskData = await NoteMongodb.find(noteId)
+router.post("/update", async ctx => {
+  ctx.isStrings(["noteId", "title", "content"]);
+  const { noteId, title, content } = ctx.vals;
+  if (!(await NoteMongodb.find(noteId))) {
+    throw { code: 10002, msg: "笔记不存在" };
+  }
+  const updateTime = new Date().getTime();
+  const taskData = await NoteMongodb.update(noteId, title, content, updateTime);
+  if (!taskData.ok) {
+    throw { code: 500, msg: "修改失败~" };
+  }
+  ctx.sendSuccess("", "修改成功~");
+});
+
+router.post("/detail", async ctx => {
+  ctx.isStrings(["noteId"]);
+  const { noteId } = ctx.vals;
+  const taskData = await NoteMongodb.find(noteId);
   if (!taskData) {
-    throw { code: 500, msg: '任务不存在' }
+    throw { code: 500, msg: "任务不存在" };
   }
   const data = {
     noteId: taskData._id,
     title: taskData.title,
     content: taskData.content,
     date: taskData.date,
-    priority: taskData.priority,
-  }
-  ctx.sendSuccess(data)
-})
+    priority: taskData.priority
+  };
+  ctx.sendSuccess(data);
+});
 
-router.post('/list', async (ctx) => {
-  const noteList = await NoteMongodb.list()
+router.post("/list", async ctx => {
+  const noteList = await NoteMongodb.list();
   const data = noteList.map(item => {
     return {
       noteId: item._id,
       title: item.title,
       content: item.content,
       createTime: item.createTime,
-      updateTime: item.updateTime,
-    }
+      updateTime: item.updateTime
+    };
   });
   ctx.sendSuccess(data);
-})
+});
 
-module.exports = router
+module.exports = router;
